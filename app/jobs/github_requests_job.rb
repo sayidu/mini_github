76f35f07f0ace26a76ab7a030b1f1115bfdbe5e4
Shalom Ayidu <shalom.ayidu@andela.com>
2019-05-22 17:54:40 +0100

    WIP Public Send

--- app/jobs/github_requests_job.rb
+++ app/jobs/github_requests_job.rb
@@ -2,6 +2,6 @@ class GithubRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(method_name, *request_params)
-    GithubService.send method_name.to_s, *request_params
+    GithubService.public_send method_name.to_s, *request_params
   end
 end
